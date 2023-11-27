8b7e6478fd091ce1b6167b75ec126e63be369982
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-11-27 13:00:22 +0900

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -7,20 +7,32 @@ connection = pymysql.connect(host='localhost',
                              database='mydb',
                              cursorclass=pymysql.cursors.DictCursor)
 
-with connection:
-    with connection.cursor() as cursor:
-        # Create a new record
-        sql = "INSERT INTO users (email, password) VALUES (%s, %s)"
-        sql = "INSERT INTO SensorData (sensor_id, reading, timestamp) VALUES (%s, %s, CURRENT_TIMESTAMP)"
-        cursor.execute(sql, (2, 30.5))
+# Assume the received data is in the format: "sensor_id reading\n"
+def parse_sensor_data(data):
+    sensor_id, reading = data.strip().split()
+    return int(sensor_id), float(reading)
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    connection.commit()
+# Modify this part to read from FIFO and process the received data
+with open('fifo', 'r') as fifo_file:
+    while True:
+        line = fifo_file.readline().strip()  # Read a line from FIFO
+        if not line:
+            continue
 
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "select b.id, a.name, b.sensor_id, b.reading, b.timestamp, c.status from Sensors as a, SensorData as b, SensorStatus as c WHERE a.id=b.sensor_id and a.id=c.sensor_id and a.name=%s"
-        cursor.execute(sql, ('Sensor2',))
-        result = cursor.fetchone()
-        print(result)
+        sensor_id, reading = parse_sensor_data(line)  # Parse the received data
+        timestamp = None  # Since the timestamp is automatically added, no need to specify here
+
+        with connection.cursor() as cursor:
+            # Insert the parsed data into SensorData table
+            sql = "INSERT INTO SensorData (sensor_id, reading, timestamp) VALUES (%s, %s, CURRENT_TIMESTAMP)"
+            cursor.execute(sql, (sensor_id, reading))
+
+        # Commit the changes to the database
+        connection.commit()
+
+        # Display the inserted data
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM SensorData WHERE sensor_id = %s AND reading = %s"
+            cursor.execute(sql, (sensor_id, reading))
+            result = cursor.fetchone()
+            print(result)
