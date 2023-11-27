a55e986f9158310b60c6d052fa97512c2363189a
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-11-27 12:56:40 +0900

    Update server.c

--- server.c
+++ server.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <unistd.h> 			/* read(), close(), unlink() 등의 시스템 콜을 위한 헤더 파일 */
-#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define FIFOFILE "fifo"
+#define PYTHON_SCRIPT "mydb.py" // 파이썬 스크립트 파일명
 
-int main(int argc, char **argv)
-{
-    int n, fd;
+int main(int argc, char **argv) {
+    int fd;
     char buf[BUFSIZ];
-    unlink(FIFOFILE); 			/* 기존의 FIFO 파일을 삭제한다. */
 
-    if(mkfifo(FIFOFILE, 0666) < 0) { 	/* 새로운 FIFO 파일을 생성한다. */
+    unlink(FIFOFILE); // 기존 FIFO 파일 삭제
+
+    if (mkfifo(FIFOFILE, 0666) < 0) { // 새로운 FIFO 파일 생성
         perror("mkfifo()");
         return -1;
     }
 
-    if((fd = open(FIFOFILE, O_RDONLY)) < 0) { 		/* FIFO를 연다. */
+    if ((fd = open(FIFOFILE, O_RDONLY)) < 0) { // FIFO를 읽기 전용으로 열기
         perror("open()");
         return -1;
     }
 
-    while((n = read(fd, buf, sizeof(buf))) > 0) 	/* FIFO로부터 데이터를 받아온다. */
-        printf("%s", buf); 		/* 읽어온 데이터를 화면에 출력한다. */
+    FILE *fp;
+    char command[BUFSIZ];
+
+    while (1) {
+        int n = read(fd, buf, sizeof(buf)); // FIFO로부터 데이터를 읽음
+
+        if (n > 0) {
+            buf[n] = '\0'; // Null-terminate 문자열
+
+            // 파이썬 스크립트를 호출하여 FIFO로부터 받은 데이터를 전달하고 MariaDB에 데이터 삽입
+            sprintf(command, "python3 %s \"%s\"", PYTHON_SCRIPT, buf);
+            fp = popen(command, "r");
+            if (fp == NULL) {
+                printf("Failed to execute Python script.\n");
+                return -1;
+            }
+            pclose(fp);
+        }
+    }
 
     close(fd);
-
     return 0;
 }
