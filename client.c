8ad9272916122c2970b4ef3444ca3dd381bbb586
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-11-27 11:30:11 +0900

    Update client.c

--- client.c
+++ client.c
@@ -18,17 +18,17 @@ int main(int argc, char **argv)
 
     while (1) {
         // 랜덤 텍스트 생성
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < BUFSIZ; ++i) {
             buf[i] = 'A' + rand() % 26; // 랜덤 알파벳 생성
         }
-        buf[3] = '\0'; // 문자열 종료
+        buf[BUFSIZ] = '\0'; // 문자열 종료
 
         // FIFO로 데이터 보내기
         write(fd, buf, BUFSIZ);
 
         fflush(stdout); // 표준 출력 버퍼 비우기
 
-        sleep(1); // 1초 대기
+        sleep(2); // 2초 대기
     }
 
     return 0;
