5a4f2ca5001b767fc2dbf045a9f3adc66ae410e9
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-02 20:57:05 +0900

    Update makedb.py

--- makedb.py
+++ makedb.py
@@ -3,21 +3,13 @@ import pymysql
 # MariaDB 연결 설정
 conn = pymysql.connect(
     host='localhost',
-    user='scott',  # root 계정으로 접속합니다. 적절한 권한을 가진 계정으로 변경하세요.
-    password='tiger',  # root 계정의 비밀번호를 입력하세요.
+    user='scott',
+    password='tiger',
+    database='exam'  # 이미 생성된 exam 데이터베이스 선택
 )
-
 
 # 커서 생성
 cursor = conn.cursor()
-
-# exam 데이터베이스 생성
-cursor.execute("GRANT ALL PRIVILEGES ON exam.* TO 'scott'@'localhost'")
-cursor.execute("CREATE DATABASE IF NOT EXISTS exam")
-print("exam 데이터베이스가 생성되었습니다.")
-
-# exam 데이터베이스 선택
-cursor.execute("USE exam")
 
 # Sensors 테이블 생성
 cursor.execute("""
@@ -55,9 +47,6 @@ cursor.execute("""
     )
 """)
 print("Sensorstatus 테이블이 생성되었습니다.")
-
-# 'scott' 사용자에게 'exam' 데이터베이스에 대한 권한 부여
-cursor.execute("GRANT ALL PRIVILEGES ON exam.* TO 'scott'@'localhost'")
 
 # 커밋 및 연결 종료
 conn.commit()
