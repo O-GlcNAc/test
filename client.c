9ca86bd433307aa58f00fea6f8512076b4831f61
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 14:14:46 +0900

    Update client.c

--- client.c
+++ client.c
@@ -23,41 +23,21 @@ int main(int argc, char **argv) {
 
     time_t current_time = mktime(&start_time); // 시작 시간을 time_t 형식으로 변환
 
+    char timestamp_str[20]; // MariaDB timestamp 형식으로 저장할 문자열 버퍼
+
     while (1) {
-        int sensor_id = rand() % 3 + 1; // 1에서 3 사이의 랜덤 sensor_id 생성
+        // ...
 
-        int timestamp = (int)current_time; // 현재 시간을 timestamp로 사용
+        strftime(timestamp_str, sizeof(timestamp_str), "%Y-%m-%d %H:%M:%S", localtime(&current_time));
 
-        int temperature, humidity, illuminance;
+        // FIFO로 sensor_id, timestamp_str, temperature, humidity, illuminance 데이터 전송
+        dprintf(fd, "%d %s %d %d %d\n", sensor_id, timestamp_str, temperature, humidity, illuminance);
 
-        // sensor_id에 따라 온도, 습도, 조도 값을 설정
-        if (sensor_id == 1) {
-            temperature = rand() % 61 - 20; // sensor_id 1의 온도는 -20에서 40 사이 값
-            humidity = rand() % 101;        // sensor_id 1의 습도는 0에서 100 사이 값
-            illuminance = rand() % 1001;    // sensor_id 1의 조도는 0에서 1000 사이 값
-        } else if (sensor_id == 2) {
-            temperature = rand() % 101; // sensor_id 2의 온도는 0에서 100 사이 값
-            humidity = rand() % 101;    // sensor_id 2의 습도는 0에서 100 사이 값
-            illuminance = rand() % 1001; // sensor_id 2의 조도는 0에서 1000 사이 값
-        } else {
-            temperature = rand() % 21; // sensor_id 3의 온도는 0에서 20 사이 값
-            humidity = rand() % 101;   // sensor_id 3의 습도는 0에서 100 사이 값
-            illuminance = rand() % 1001; // sensor_id 3의 조도는 0에서 1000 사이 값
-        }
+        // ...
 
-        // FIFO로 sensor_id, timestamp, temperature, humidity, illuminance 데이터 전송
-        dprintf(fd, "%d %d %d %d %d\n", sensor_id, timestamp, temperature, humidity, illuminance);
-
-        // 3분씩 증가
         current_time += 180; // 180초는 3분
 
-        // 0.3초 대기
-        usleep(300000); // 300000 마이크로초 = 0.3초
-
-        // 23년 12월 2일 00시까지의 데이터를 생성한 후 종료
-        if (current_time >= mktime(&start_time) + 86400) { // 86400초는 하루의 초 단위 값
-            break;
-        }
+        // ...
     }
 
     close(fd);
