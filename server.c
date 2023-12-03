764dc81183868eace108c4fcbc65c8bf3be91d13
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 11:47:27 +0900

    Update server.c

--- server.c
+++ server.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h> // 오류 메시지 출력을 위해 추가
 
 #define FIFOFILE "fifo"
-#define PYTHON_SCRIPT "mydb.py" // 파이썬 스크립트 파일명
+#define PYTHON_SCRIPT "mydb.py" // 'mydb.py' 파일의 절대 경로로 변경
 
 int main(int argc, char **argv) {
     int fd;
@@ -34,13 +35,13 @@ int main(int argc, char **argv) {
             // 파이썬 스크립트를 호출하여 FIFO로부터 받은 데이터를 전달하고 MariaDB에 데이터 삽입
             fp = popen(PYTHON_SCRIPT, "w"); // 쓰기 전용으로 파일 포인터 열기
             if (fp == NULL) {
-                printf("Failed to execute Python script.\n");
+                fprintf(stderr, "Failed to execute Python script: %s\n", strerror(errno)); // 오류 메시지 출력
                 return -1;
             }
 
             // FIFO로부터 받은 데이터를 파이썬 스크립트에 쓰기
             fwrite(buf, sizeof(char), strlen(buf), fp);
-            fclose(fp); // 파일 포인터 닫기
+            pclose(fp); // 파일 포인터 닫기
 
             // 파이썬 스크립트의 실행 결과는 터미널에서 직접 확인할 수 있도록 변경하였습니다.
             printf("Data sent to Python script for database insertion.\n");
