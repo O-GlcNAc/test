2d1c98d28bbff15dbcb1b7df55eec935b3316390
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 12:08:53 +0900

    어제 server.c

--- server.c
+++ server.c
@@ -3,10 +3,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h> // 오류 메시지 출력을 위해 추가
 
 #define FIFOFILE "fifo"
-#define PYTHON_SCRIPT "mydb.py" // 'mydb.py' 파일의 절대 경로로 변경
+#define PYTHON_SCRIPT "mydb.py" // 파이썬 스크립트 파일명
 
 int main(int argc, char **argv) {
     int fd;
@@ -25,6 +24,7 @@ int main(int argc, char **argv) {
     }
 
     FILE *fp;
+    char command[BUFSIZ];
 
     while (1) {
         int n = read(fd, buf, sizeof(buf)); // FIFO로부터 데이터를 읽음
@@ -33,18 +33,19 @@ int main(int argc, char **argv) {
             buf[n] = '\0'; // Null-terminate 문자열
 
             // 파이썬 스크립트를 호출하여 FIFO로부터 받은 데이터를 전달하고 MariaDB에 데이터 삽입
-            fp = popen(PYTHON_SCRIPT, "w"); // 쓰기 전용으로 파일 포인터 열기
+            sprintf(command, "python3 %s \"%s\"", PYTHON_SCRIPT, buf);
+            fp = popen(command, "r");
             if (fp == NULL) {
-                fprintf(stderr, "Failed to execute Python script: %s\n", strerror(errno)); // 오류 메시지 출력
+                printf("Failed to execute Python script.\n");
                 return -1;
             }
+            char result_buffer[1024]; // 출력 결과를 담을 버퍼
 
-            // FIFO로부터 받은 데이터를 파이썬 스크립트에 쓰기
-            fwrite(buf, sizeof(char), strlen(buf), fp);
-            pclose(fp); // 파일 포인터 닫기
+            while (fgets(result_buffer, sizeof(result_buffer), fp) != NULL) {
+                printf("%s", result_buffer); // 파이썬 스크립트의 출력 결과를 터미널에 출력
+            }
 
-            // 파이썬 스크립트의 실행 결과는 터미널에서 직접 확인할 수 있도록 변경하였습니다.
-            printf("Data sent to Python script for database insertion.\n");
+            pclose(fp);
         }
     }
 
